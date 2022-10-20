fc5d78a19e6e8d016a30779f2118149bb01c26b5
Mehmet Mert Denizman <amorylorch@gmail.com>
2022-10-20 23:39:21 +0300

    Add files via upload

---
+++ Services/JsonFileProductService.cs
@@ -0,0 +1,28 @@
+﻿using ContosoCrafts.WebSite.Models;
+using System.Security.AccessControl;
+using System.Text.Json;
+
+namespace ContosoCrafts.WebSite.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+
+        }
+        public IWebHostEnvironment WebHostEnvironment { get; set; }
+
+
+        private string JsonFileName { get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "product.json"); } }
+
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd());
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Builder;
+
+namespace ContosoCrafts.WebSite
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddTransient<JsonFileProductService>();
+        }
+    }
+}
